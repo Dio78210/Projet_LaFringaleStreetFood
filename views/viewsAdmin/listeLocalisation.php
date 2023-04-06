82ffc154bae03fb25b7d6159f9c74471a9fb2f63
Dio78210 <damienmezieres@gmail.com>
2023-04-06 10:56:11 +0200

    modification des images des menus

--- views/viewsAdmin/listeLocalisation.php
+++ views/viewsAdmin/listeLocalisation.php
@@ -3,35 +3,36 @@
     <!-- <a href="/Admin/galeriePrivatisation.php " class="btn btn-success m-5">Revenir a l'ajout de photo</a> -->
 
     <!-- faire un tableau pour afficher les infos de la BDD et enregistrer a la main les premieres info dans la BDD -->
-
-    <table class="table caption-top">
-        <h1>Liste des emplacements</h1>
-        <thead>
-            <tr>
-                <th scope="col">Jour</th>
-                <th scope="col">Heure de début</th>
-                <th scope="col">Heure de fin</th>
-                <th scope="col">Lieu</th>
-                <th scope="col">Coordonée Latitude (x)</th>
-                <th scope="col">Coordonée Longitude (y)</th>
-        </thead>
-        <tbody>
-            <?php
-            foreach ($localisations as $localisation) { ?>
+    <div class="container">
+        <table class="table">
+            <h1>Liste des emplacements</h1>
+            <thead>
                 <tr>
-                    <td><?= $localisation->jour_semaine ?></td>
-                    <td><?= $localisation->heure_debut ?></td>
-                    <td><?= $localisation->heure_fin ?></td>
-                    <td><?= $localisation->lieu ?></td>
-                    <td><?= $localisation->x ?></td>
-                    <td><?= $localisation->y ?></td>
-                    <td><a href="/Admin/modifierLocalisation.php?id=<?= $localisation->id_localisation ?>">Modifier</a></td>
-                </tr>
-            <?php
-            }
-            ?>
-        </tbody>
-    </table>
+                    <th scope="col">Jour</th>
+                    <th scope="col">Heure de début</th>
+                    <th scope="col">Heure de fin</th>
+                    <th scope="col">Lieu</th>
+                    <th scope="col">Coordonée Latitude (x)</th>
+                    <th scope="col">Coordonée Longitude (y)</th>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($localisations as $localisation) { ?>
+                    <tr>
+                        <td><?= $localisation->jour_semaine ?></td>
+                        <td><?= $localisation->heure_debut ?></td>
+                        <td><?= $localisation->heure_fin ?></td>
+                        <td><?= $localisation->lieu ?></td>
+                        <td><?= $localisation->x ?></td>
+                        <td><?= $localisation->y ?></td>
+                        <td><a href="/Admin/modifierLocalisation.php?id=<?= $localisation->id_localisation ?>">Modifier</a></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 
 
 </div>
